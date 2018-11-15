82740f2170b2157892d58a9d36ff5427323886f2
Daniel Burbach and Quinn Miller <daniel@>
2018-11-15 15:34:13 -0800

    changed the pass logic test number two recognizes 5 as prime

--- lib/prime_sifting.rb
+++ lib/prime_sifting.rb
@@ -1,16 +1,33 @@
 class Integer
+  # def initialize
+  #   @cool_thing = []
+  # end
+
+  def sift(array)
+    if (self == 0)||(self == 1)||(self % 2)==0
+      puts "not prime"
+    elsif (self == 5)
+      puts "prime"
+      array.push(self)
+    end
+  end
+
   def prime
     entered_number = self
     counter = 0
     new_array = []
+    cool_thing = []
     loop do
       new_array.push(counter)
       counter += 1
-      if counter >= entered_number
+      if counter > entered_number
         break
       end
     end
+    new_array.each do |x|
+      x.sift(cool_thing)
+    end
     binding.pry
-    ##if ((self % 2)== 0)&&(self != 2)
+    return cool_thing
   end
 end
--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -5,9 +5,9 @@ require('prime_sifting.rb')
 
 describe '#prime' do
   it("recognizes 2 as a prime number") do
-    expect(2.prime).to eql(2)
+    expect(2.prime).to eql([2])
   end
   it("recognizes 3 as a prime number") do
-    expect(4.prime).to eql(2)
+    expect(11.prime).to eql([2,3,4,5,6,7,8,9,10])
   end
 end
